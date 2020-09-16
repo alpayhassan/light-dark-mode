44f42ce54d5152101cab1e42e78a5381b577fd8a
Alpay Hassan <alpayhassan@hotmail.com>
2020-09-16 14:42:41 +0100

    cleaned up the code

--- script.js
+++ script.js
@@ -2,21 +2,15 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
 const image1 = document.getElementById('image1');
-// const image2 = document.getElementById('image2');
-// const image3 = document.getElementById('image3');
-// const textBox = document.getElementById('text-box');
 const mainTitle = document.getElementById('page-title');
 
 // Dark or Light Images
 function imageMode(color) {
   image1.src = `img/undraw_lightbulb_moment_${color}.svg`;
-  // image2.src = `img/undraw_feeling_proud_${color}.svg`;
-  // image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
 function toggleDarkLightMode(isDark) {
   nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
-  // textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
   toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
   mainTitle.textContent = isDark ? "It's Too Dark In Here" : "It's Too Bright In Here";
   isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
