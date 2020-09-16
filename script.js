6b9480894048e3b05d217746676c9bc9314478aa
Alpay Hassan <alpayhassan@hotmail.com>
2020-09-16 14:30:55 +0100

    changed image in about section

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const mainTitle = document.getElementById('page-title');
 
 // Dark or Light Images
 function imageMode(color) {
-  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image1.src = `img/undraw_lightbulb_moment_${color}.svg`;
   // image2.src = `img/undraw_feeling_proud_${color}.svg`;
   // image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
